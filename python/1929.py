3af2e4ebab08721dcf506d272b83ace597f4b3d2
jeonggiju <halogiju123@gmail.com>
2025-02-03 16:24:39 +0900

    2025/2/3

---
+++ python/1929.py
@@ -0,0 +1,14 @@
+def isPrime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n,m = map(int, input().split())
+
+for i in range(n,m+1):
+    if isPrime(i):
+        print(i)
